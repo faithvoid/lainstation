ec22ebabff90eb7f5dadd1f2cf6363e8c0f732a7
Rachel Bartmoss <56975081+faithvoid@users.noreply.github.com>
2026-02-07 04:49:29 -0500

    Create autostart.sh

---
+++ opt/retropie/configs/all/autostart.sh
@@ -0,0 +1,4 @@
+omxplayer --no-osd --win "0 0 720 480" /home/pi/RetroPie/splashscreens/retropie.mp4 > /dev/null 2>&1 # Playstation-style startup video, 480p
+# python3 /home/pi/scripts/sakuraDisplay.py &  # sakuraDisplay SPI display script
+# python3 /home/pi/scripts/sakuraPresence.py &  # sakuraPresence Discord Rich Presence script
+emulationstation --no-splash #auto (sans splash screen)
